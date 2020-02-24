9030937fab0df0b80eea807b66f6ba17f953eed5
Lucas A <lucwars@hotmail.com>
2020-02-23 22:55:54 -0300

    Segundo Desenvolvimento
    adicionado sprites dos inimigos(gatos), adicionado animação de sprites nos inimigos,  adicionado a função de seguir o player.

---
+++ Cat Defense/Assets/Script/Enemy_Scripts/EnemyFollow.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+
+    public float speed;
+
+    private Transform playerPos;
+    
+    void Awake()
+    {
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform; // assim que começar ele ira logo atras da tag "Player"(Object)
+    }
+
+    
+    void Update()
+    {
+        if(Vector2.Distance(transform.position, playerPos.position)> 0.3f) //caso ele encontre o player ele fica a 0.3f de distancia do player
+        transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);//calculando a posição em que o player se move para segui-lo
+    }
+}
